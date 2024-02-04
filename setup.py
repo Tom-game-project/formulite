0b4185fed292cec72499f3540ca3888f5bc4a67f
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-02-04 19:31:10 +0900

    v0.0.4 apdate

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md",encoding="utf-8",mode="r")as f:
 
 setup(
     name="formulite",
-    version="0.0.3",
+    version="0.0.4",
     description='Simple Formula Parser',
     long_description=long_description,
     long_description_content_type='text/markdown',
